4ea8daef108003f98a2fc3ab36a2068f484e197e
Jakub Beran <nomail>
2021-12-11 21:22:24 +0100

    Day 11 solution using numpy

--- day11_1.py
+++ day11_1.py
@@ -1,33 +1,51 @@
-from statistics import median
+import numpy as np
+from numpy import asarray as ar
 
 # Define path
-data_path = "data/input10"
-
-point_map = {'(':1,'[':2,'{':3,'<':4}
-match_map = {')':'(',']':'[','}':'{','>':'<'}
-points = []
-
-def verify_line(line, match_map):
-    stack = []
-    for c in line:
-        if c in '([{<':
-            stack.append(c)
-        else:
-            if match_map[c] != stack.pop():
-                return 'Error'
-    if len(stack) == 0:
-        return 'OK'
-    else: 
-        return stack
+data_path = "data/input11"
+data = []
 
 # Read line-by-line
 f = open(data_path, "r")
 for x in f:
-    out = verify_line(x.strip(), match_map)
-    if out not in ['Error', 'OK']:
-        p_line = 0
-        for i in range(len(out) - 1, -1, -1):
-            p_line = 5 * p_line + point_map[out[i]]
-        points.append(p_line)
-            
-print(f'Middle score is {median(points)}')
+    data.append([int(c) for c in x.strip()])
+    
+# Convert to numpy
+data = np.array(data)
+dim = len(data)
+steps = 100
+    
+def do_step(data, l=10):
+    cnt = 0
+    data = data + 1
+    while sum(sum(data > 9)) > 0:
+        cnt += sum(sum(data > 9))
+        indices10 = np.where(data > 9)
+        data[np.where(data > 9)] = -100
+        for (i,j) in list(zip(*indices10)):
+            ia = (
+                np.array([i-1,i-1,i-1,i,i,i+1,i+1,i+1]), 
+                np.array([j-1,j,j+1,j-1,j+1,j-1,j,j+1])
+                )
+            keep = ar(ia[0] >= 0) & ar(ia[1] >= 0) & ar(ia[0] < l) & ar(ia[1] < l) 
+            indices_clean = (ia[0][keep], ia[1][keep])
+            data[indices_clean] += 1
+    data[np.where(data < 0)] = 0
+    return data, cnt
+
+steps = 100
+count = 0
+
+for i in range(steps):
+    data, count_step = do_step(data, l=dim)
+    count += count_step
+    
+print(f'There were {count} flashes after {steps} steps')
+
+count = 0
+step = 0
+while count < 100:
+    step += 1
+    data, count = do_step(data, l=dim)
+
+print(f'All octopuses flash simultaneously in step {step + 100}')
